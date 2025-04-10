db5862f615c977030f20a68a0b7d9e4d4186bd3c
Lenkly2 <143933790+Lenkly2@users.noreply.github.com>
2025-04-10 19:04:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from objects import *
 
-window = pygame.display.set_mode((800,600))
+window = pygame.display.set_mode((600,600))
 class Tank():
     def __init__(self,hp,damage,x,y,image,w,h):
         self.image = pygame.transform.scale(pygame.image.load(image),(w,h))
@@ -19,12 +19,29 @@ class MainTank(Tank):
         key = pygame.key.get_pressed()
         if key[pygame.K_d]:
             self.rect.x +=1
+            self.image = tankimg[2]
+            for i in walls:
+                if pygame.sprite.collide_rect(self,i):
+                    self.rect.x -=1
         if key[pygame.K_a]:
             self.rect.x -=1
+            self.image = tankimg[1]
+            for i in walls:
+                if pygame.sprite.collide_rect(self,i):
+                    self.rect.x +=1
         if key[pygame.K_s]:
             self.rect.y +=1
+            self.image = tankimg[3]
+            for i in walls:
+                if pygame.sprite.collide_rect(self,i):
+                    self.rect.y -=1
         if key[pygame.K_w]:
             self.rect.y -=1
+            self.image = tankimg[0]
+            for i in walls:
+                if pygame.sprite.collide_rect(self,i):
+                    self.rect.y +=1
+            
 class NpcTank(Tank):
     def __init__(self):
         self.orint = 'right'
@@ -33,13 +50,17 @@ class NpcTank(Tank):
             self.rect.x -= 1
         if self.orint == "right":
             self.rect.x += 1
+
         if self.rect.x <= 0:
             self.orint = "right"
         if self.rect.x >= 500:
             self.orint = "left"
+
 backround = (0,0,0)
 clock = pygame.time.Clock()
-tanck1 = MainTank(50,10,0,0,"images/tank1.png",45,45)
+tankimg = ["images/tank1-f.png","images/tank1-l.png","images/tank1-r.png","images/tank1-b.png"]
+tanck1 = MainTank(50,10,0,0,tankimg[0],40,45)
+walls = [brick1,brick2,brick3,brick4,brick5,brick6]
 game = True
 while game:
     for e in pygame.event.get():
@@ -47,7 +68,8 @@ while game:
             game = False
 
     window.fill(backround)
-    window.blit(brick1,(45,45))
+    for i in walls:
+        window.blit(i.image,(i.rect.x,i.rect.y))
     tanck1.update()
     tanck1.move()
     pygame.display.update()
--- objects.py
+++ objects.py
@@ -1,2 +1,15 @@
 import pygame
-brick1 = pygame.transform.scale(pygame.image.load("images/brick1.png"),(45,250))
+
+class Image():
+    def __init__(self,x,y,image,w,h):
+        self.image = pygame.transform.scale(pygame.image.load(image),(w,h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+brick1 = Image(45,45,"images/brick1.png",45,250)
+brick2 = Image(135,45,"images/brick1.png",45,250)
+brick3 = Image(225,45,"images/brick1.png",45,205)
+brick4 = Image(315,45,"images/brick1.png",45,205)
+brick5 = Image(405,45,"images/brick1.png",45,250)
+brick6 = Image(495,45,"images/brick1.png",45,250)
